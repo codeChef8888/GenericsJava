03a57021d4e4dd88311a3d555c192da4d33f0cda
codeChef8888 <eryashre7777@gmail.com>
2021-12-27 14:39:38 +0545

    javaGenericsRevised

--- src/BoundedTypeParameters/Box.java
+++ src/BoundedTypeParameters/Box.java
@@ -22,7 +22,7 @@ public class Box<T> {
     public static void main(String[] args) {
         Box<Integer> integerBox = new Box<Integer>();
         integerBox.set(new Integer(10));
-        integerBox.inspect("some text"); // error: this is still String!
+        // integerBox.inspect("some text"); // error: this is still String!
     }
     
 
